4e4669d3a5f058e89b185aba897f20d9b9f42374
biggieliten <filipisaksson00@gmail.com>
2026-09-22 13:57:45 +0200

    Add zoom button

--- src/app/iss-map.tsx
+++ src/app/iss-map.tsx
@@ -1,5 +1,7 @@
 import { Image } from "expo-image";
+import { useState } from "react";
 import {
+  Button,
   PixelRatio,
   StyleSheet,
   View,
@@ -14,15 +16,25 @@ const LONGITUDE = 18.06;
 export default function ISSMap() {
   const { width } = useWindowDimensions();
   const pixelRatio = PixelRatio.get();
-
+  const [zoom, setZoom] = useState(1);
   const mapWidth = Math.round(width * pixelRatio);
   const mapHeight = Math.round(mapWidth * 0.66);
 
-  const mapUrl = `https://maps.geoapify.com/v1/staticmap?style=dark-matter-dark-grey&width=${mapWidth}&height=${mapHeight}&attribution=none&scaleFactor=2&center=lonlat:${LONGITUDE},${LATITUDE}&zoom=1&marker=lonlat:${LONGITUDE},${LATITUDE};type:circle;icon:satellite;icontype:awesome;contentcolor:%23ffffff;contentcolor:%2300d9ff;size:large&apiKey=${GEOAPIFY_API_KEY}`;
+  const mapUrl = `https://maps.geoapify.com/v1/staticmap?style=dark-matter-dark-grey&width=${mapWidth}&height=${mapHeight}&attribution=none&scaleFactor=2&center=lonlat:${LONGITUDE},${LATITUDE}&zoom=${zoom}&marker=lonlat:${LONGITUDE},${LATITUDE};type:circle;icon:satellite;icontype:awesome;contentcolor:%23ffffff;contentcolor:%2300d9ff;size:large&apiKey=${GEOAPIFY_API_KEY}`;
 
   return (
     <View style={styles.container}>
       <Image source={{ uri: mapUrl }} style={styles.map} contentFit="cover" />
+      <View>
+        <Button
+          title="+"
+          onPress={() => setZoom((z: number) => Math.min(z + 0.5, 20))}
+        />
+        <Button
+          title="-"
+          onPress={() => setZoom((z: number) => Math.max(z - 0.5, 0))}
+        />
+      </View>
     </View>
   );
 }
