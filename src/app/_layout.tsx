e0a4fb758b1226b6a4e9e55f0460f6a340650a78
biggieliten <filipisaksson00@gmail.com>
2026-09-24 15:27:23 +0200

    Remove boiler plate code

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
-import { useColorScheme } from "react-native";
 
 import AppTabs from "@/components/app-tabs";
 
 const client = new QueryClient();
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <QueryClientProvider client={client}>
-        <AppTabs />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <AppTabs />
+    </QueryClientProvider>
   );
 }
